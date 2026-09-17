dd0f81900020e83dc76365e9588e7dd93eee59d6
Ronald Sprouse <ronald@BERKELEY.EDU>
2026-09-16 17:24:38 -0700

    Add \medskip

--- interlinearized.py
+++ interlinearized.py
@@ -476,7 +476,7 @@ for glosslang in ('en', 'es'):
                 parallelfile.write(r'\begin{Parallel}{0.47\textwidth}{0.47\textwidth}' + '\n')
                 parallelfile.write(r' \ParallelLText{\noindent \textit{' + ' '.join(paralleltexts['left']) + '}}\n')
                 parallelfile.write(r' \ParallelRText{\noindent \textit{' + ' '.join(paralleltexts['right']) + '}}\n')
-                parallelfile.write(r'\end{Parallel}' + '\n')
+                parallelfile.write(r'\end{Parallel}' + '\n' + r'\medskip' + \n')
                 paralleltexts = {'left': [], 'right': []}
                 endparallelblock = False
             if fourline:
